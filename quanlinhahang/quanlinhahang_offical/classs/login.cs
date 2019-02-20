a35cd39663b2f96231bc8df94b033c034fe13837
hoangahh98 <hoangahh98@gmail.com>
2019-02-20 09:32:27 +0700

    vi sao

---
+++ quanlinhahang/quanlinhahang_offical/classs/login.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using quanlinhahang_offical.classs;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Sql;
+
+namespace quanlinhahang_offical.classs
+{
+    class login
+    {
+        connect cn = new connect();
+        
+        public bool isLogin(string username , string password )
+        {
+            bool kp = false;
+            if (cn.isConnect())
+            {
+                SqlCommand cmd = new SqlCommand();
+                
+                cmd.CommandText = @"checkaccount";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Connection = cn.cnt;
+                cmd.Parameters.AddWithValue("@user", username);
+                cmd.Parameters.AddWithValue("@pass", password);
+
+                //int i = cmd. ExecuteNonQuery();
+                int i = Convert.ToInt32(cmd.ExecuteScalar());
+                if (i > 0)
+                    {
+                        kp = true;
+                    }
+                    else
+                    {
+                        kp = false;
+                    }
+              
+               
+            }
+            return kp;
+            
+        }
+    }
+}
